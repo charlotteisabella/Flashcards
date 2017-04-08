6c0ff7719ab50483b96653a59046faef6c7c180f
Charlotte McCarthy <charlotteimccarthy@gmail.com>
2017-04-08 19:54:47 +1000

    Set up eslint

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
-import React from "react"
-import HelloWorld from "./components/hello_world"
+import React from 'react';
+import HelloWorld from './components/hello_world';
 
 export default function App() {
-  return(
+  return (
     <div>
       <HelloWorld />
     </div>
